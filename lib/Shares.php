f866e1b93d8ff17fd89da2380679978d97ca6805
mbassan <moises@organic.com.pa>
2015-11-29 17:53:05 -0500

    Share screen - history view.

--- lib/Shares.php
+++ lib/Shares.php
@@ -32,19 +32,58 @@ class Shares {
 		$r1 = $page * $per_page;
 	
 		if (!$count)
-			$sql = 'SELECT id,`date`,shares_owned,percentage_owned,(dividend / '.$currency_info['usd_ask'].') AS dividend';
+			$sql = 'SELECT id,`date`,shares_owned,percentage_owned,dividend, currency ';
 		else
 			$sql = 'SELECT COUNT(dividends.id) AS total ';
 	
 		$sql .= "
 		FROM dividends
 		WHERE site_user = ".User::$info['id'];
+		
+		if ($currency)
+			$sql .= ' AND dividends.currency = '.$currency_info['id'];
 	
 		if ($per_page > 0 && !$count)
 			$sql .= " ORDER BY id DESC LIMIT $r1,$per_page ";
 	
 		$result = db_query_array($sql);
+		if (!$count)
+			return $result;
+		else
+			return $result[0]['total'];
+	}
 	
+	public static function getHistory($count=false,$page=false,$per_page=false,$currency=false) {
+		global $CFG;
+		
+		if (!$CFG->session_active)
+			return false;
+		
+		$page = preg_replace("/[^0-9]/", "",$page);
+		$per_page = preg_replace("/[^0-9]/", "",$per_page);
+		$currency = preg_replace("/[^a-zA-Z]/", "",$currency);
+		$currency_info = (!empty($CFG->currencies[strtoupper($currency)])) ? $CFG->currencies[strtoupper($currency)] : $CFG->currencies['USD'];
+		
+		$page = ($page > 0) ? $page - 1 : 0;
+		$r1 = $page * $per_page;
+		
+		if (!$count)
+			$sql = 'SELECT shares_log.* ,history_actions.name_'.$CFG->language.' AS type ';
+		else
+			$sql = 'SELECT COUNT(shares_log.id) AS total ';
+		
+		$sql .= "
+		FROM shares_log
+		LEFT JOIN history_actions ON (shares_log.history_action = history_actions.id)
+		WHERE shares_log.site_user = ".User::$info['id'];
+		
+		if ($currency)
+			$sql .= ' AND shares_log.currency = '.$currency_info['id'];
+		
+		if ($per_page > 0 && !$count)
+			$sql .= " ORDER BY shares_log.id DESC LIMIT $r1,$per_page ";
+		
+		$result = db_query_array($sql);
 		if (!$count)
 			return $result;
 		else
@@ -86,10 +125,12 @@ class Shares {
 		User::updateBalances(User::$info['id'],array($currency=>($this_balance + ($multiplier * $total))));	
 		db_commit();
 		
+		self::updateImbalance($currency_info['id'],($total * $multiplier1));
 		self::updateHeld($shares * $multiplier1);
 		User::deleteCache();
 		User::deleteBalanceCache(User::$info['id']);
 		db_insert('history',array('date'=>date('Y-m-d H:i:s'),'ip'=>$CFG->client_ip,'history_action'=>(($buy) ? $CFG->history_buy_shares_id : $CFG->history_sell_shares_id),'site_user'=>User::$info['id'],'balance_before'=>$this_balance,'balance_after'=>($this_balance + ($multiplier * $total))));
+		db_insert('shares_log',array('date'=>date('Y-m-d H:i:s'),'history_action'=>(($buy) ? $CFG->history_buy_shares_id : $CFG->history_sell_shares_id),'site_user'=>User::$info['id'],'shares'=>$shares,'shares_unit_price_usd'=>$unit_cost,'shares_unit_price'=>round($unit_cost / $currency_info['usd_ask'],($currency_info['is_crypto'] == 'Y' ? 8 : 2),PHP_ROUND_HALF_UP),'total'=>($total * $multiplier1),'currency'=>$currency_info['id'],'currency_usd'=>$currency_info['usd_ask']));
 		
 		return true;
 	}
@@ -141,6 +182,19 @@ class Shares {
 		
 		$sql = 'UPDATE shares SET shares_held = (shares_held + ('.$amount.')) WHERE id = 1';
 		return db_query($sql);
+	}
+	
+	public static function updateImbalance($currency_id,$amount) {
+		if (!$amount)
+			return false;
+		
+		$sql = 'SELECT id FROM shares_imbalances WHERE currency = '.$currency_id;
+		$result = db_query_array($sql);
+		if (!$result)
+			return db_insert('shares_imbalances',array('imbalance'=>$amount,'currency'=>$currency_id));
+		
+		$sql = 'UPDATE shares_imbalances SET imbalance = imbalance + ('.$amount.') WHERE currency = '.$currency_id;
+		return db_query_array($sql);
 	}
 }
 ?>
